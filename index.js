11c5a9b87e5630546f4ef7ff51d71788b1ff0d0d
james <jamesid2915@gmail.com>
2025-11-04 18:25:22 +0100

    Third refactor JS

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
 
 const inputEl = document.getElementById("input-el")
 const convertBtn = document.getElementById("convert-btn")
@@ -13,50 +8,7 @@ const massEl = document.getElementById("mass")
 
 const errorTextEl = document.getElementById("error-text")
 
-
-
-
-convertBtn.addEventListener("click", function(){
-    
-    const rawValue = inputEl.value.trim()
-    let value = Number(rawValue)
-
-    if(isNaN(value) || rawValue === "" || Number(value) < 0){
-        reset()
-        errorTextEl.textContent = `Please enter a valid number` 
-        return
-    }
-
-
-    if (!isNaN(value)){
-        errorTextEl.textContent = ""
-        // Length Conversion
-        const metersToFeet = convertLength("meter", value)
-        const feetToMeters = convertLength("feet", value)
-
-        // Volume Conversion
-        const litersToGallon = convertVolume("liters", value)
-        const gallonToLiters = convertVolume("gallons", value)
-
-
-        // Mass Conversion
-        const kilosToPounds = convertMass("kilos", value)
-        const poundsToKilos = convertMass("pounds", value)
-
-        lengthEl.textContent = `${value} meters = ${metersToFeet} feet | ${value} feet = ${feetToMeters} meters`
-        volumeEl.textContent = `${value} liters = ${litersToGallon} gallons | ${value} gallons = ${gallonToLiters} liters`
-        massEl.textContent = `${value} kilos = ${kilosToPounds} pounds | ${value} pounds = ${poundsToKilos} kilos`   
-    } 
-
-    
-})
-
-function reset(){
-    lengthEl.textContent = ""
-    volumeEl.textContent = ""
-    massEl.textContent = ""
-}
-
+// Press enter key to submit also
 inputEl.addEventListener("keydown", function(e){
     if(e.key === "Enter"){
         convertBtn.click()
@@ -64,39 +16,52 @@ inputEl.addEventListener("keydown", function(e){
 })
 
 
+convertBtn.addEventListener("click", function(){
+    
+    const rawValue = inputEl.value.trim()
+    let value = Number(rawValue)
 
-
-
-
-function convertLength(metric,val){
-    if (metric === "meter"){
-        return (val * 3.28084).toFixed(3)
+    if(isNaN(value) || rawValue === "" || value < 0){
+        resetOutput()
+        errorTextEl.textContent = `Please enter a valid number` 
+        return
     }
 
-    else{
-        return (val * 0.3048).toFixed(3)
-    }
+    convertUnits(value)
+    errorTextEl.textContent = ""      
+})
+
+
+
+function resetOutput(){
+    lengthEl.textContent = ""
+    volumeEl.textContent = ""
+    massEl.textContent = ""
 }
 
-function convertVolume(metric,val){
-    if (metric === "liters"){
-        return (val *  0.264172).toFixed(3)
-    }
 
-    else{
-        return (val * 3.78541).toFixed(3)
-    }
+
+
+function convertUnits(value){
+    
+    // Length Conversion
+    const metersToFeet = (value * 3.28084).toFixed(3)
+    const feetToMeters = (value * 0.3048).toFixed(3)
+    lengthEl.textContent = `${value} meters = ${metersToFeet} feet | ${value} feet = ${feetToMeters} meters`
+
+
+    // Volume Conversion
+    const litersToGallon = (value *  0.264172).toFixed(3)
+    const gallonToLiters = (value * 3.78541).toFixed(3)
+    volumeEl.textContent = `${value} liters = ${litersToGallon} gallons | ${value} gallons = ${gallonToLiters} liters`
+
+    // Mass Conversion
+    const kilosToPounds = (value *  2.20462).toFixed(3)
+    const poundsToKilos = (value * 0.453592).toFixed(3)
+    massEl.textContent = `${value} kilos = ${kilosToPounds} pounds | ${value} pounds = ${poundsToKilos} kilos` 
 }
 
-function convertMass(metric,val){
-    if (metric === "kilos"){
-        return (val *  2.20462).toFixed(3)
-    }
 
-    else{
-        return (val * 0.453592).toFixed(3)
-    }
-}
 
 
 
